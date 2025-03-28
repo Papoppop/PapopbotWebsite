95f9416395bc51371fd21e2593940f9575478c18
papoppop <47813.papop@gmail.com>
2025-03-28 22:29:24 +0700

    make images nullable

--- server/migrations/20250323182541-add-new-column-to-image.js
+++ server/migrations/20250323182541-add-new-column-to-image.js
@@ -2,11 +2,73 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('developers_images', 'devid', {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'devsticker1', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'devsticker2', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'devsticker3', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'mouthopen', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'image', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'profileWafer', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+    await queryInterface.changeColumn('developers_images', 'profile', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('developers_images', 'devid', {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'devsticker1', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'devsticker2', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'devsticker3', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'mouthopen', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'image', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'profileWafer', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.changeColumn('developers_images', 'profile', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
   }
 };
